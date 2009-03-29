babc279b1edc3942007eb9a39d164771225f3ab7
supercleanse <supercleanse@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-29 01:06:58 +0000

    Made some cleanup modifications
    git-svn-id: https://plugins.svn.wordpress.org/pretty-link/trunk@105978 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pretty-link.php
+++ pretty-link.php
@@ -3,7 +3,7 @@
 Plugin Name: Pretty Link
 Plugin URI: http://blairwilliams.com/pretty-link
 Description: Creates shortened, named URLs using your blog. This plugin makes the best possible links for your affiliate campaigns or other endeavors because the links appear to be coming from your site instead of from tinyurl, budurl or one of the other link shrinking services. You can track stats on your links easily and keep that data on your own database (instead of allowing it to be managed on other machines).
-Version: 0.0.1
+Version: 0.0.2
 Author: Blair Williams
 Author URI: http://blairwilliams.com/
 Copyright: 2009, Blair Williams
@@ -36,127 +36,6 @@ function prli_menu() {
 //      add_submenu_page(PRLI_PATH.'/prli-main.php', 'Pretty Link | Links', 'Links', 8, PRLI_PATH.'/prli-links.php');
 //      add_submenu_page(PRLI_PATH.'/prli-main.php', 'Pretty Link | Reports', 'Reports', 8, PRLI_PATH.'/prli-links.php');
 }
-
-/********* ADD LINK FIELDS ***********/
-/*
-function prli_add_custom_box() {
-  add_meta_box( 'prli_1', __( 'Pretty Link', 'prli_pretty_link' ), 
-                'prli_custom_box', 'link', 'advanced', 'high');
-
-}
-
-function prli_add_link_custom_box($link_id) {
-    global $wpdb;
-    global $wp_rewrite;
-    $pretty_links_table = $wpdb->prefix . "prli_links";
-    if( $link_id )
-    {
-      // Insert the new value into the db
-      $insert = "INSERT INTO $pretty_links_table (link_id,pretty_link,created_at) VALUES ($link_id,'".$_POST['link_pretty_link']."',NOW())";
-      $wpdb->query($insert);
-      $wp_rewrite->flush_rules();
-    }
-}
-
-function prli_edit_link_custom_box($link_id)
-{
-    global $wpdb;
-    global $wp_rewrite;
-    $pretty_links_table = $wpdb->prefix . "prli_links";
-    $select_stmt = "SELECT * FROM $pretty_links_table WHERE link_id=".$link_id." LIMIT 1";
-    $pretty_link = $wpdb->get_results($select_stmt);
-    if( $link_id )
-    {
-      if($pretty_link == null)
-      {
-        // Insert the new value into the db
-        $insert = "INSERT INTO $pretty_links_table (link_id,pretty_link,created_at) VALUES ($link_id,'".$_POST['link_pretty_link']."',NOW())";
-        $wpdb->query($insert);
-        $wp_rewrite->flush_rules();
-      }
-      else
-      {
-        // Update the record
-        $update = "UPDATE $pretty_links_table SET pretty_link='".$_POST['link_pretty_link']."' WHERE link_id=".$link_id;
-        $wpdb->query($update);
-        $wp_rewrite->flush_rules();
-      }
-    }
-}
-
-function prli_custom_box() {
-    global $link, $wpdb;
-    $base_url = get_option('siteurl');
-    $pretty_links_table = $wpdb->prefix . "prli_links";
-    $pretty_link = $wpdb->get_var("SELECT pretty_link FROM $pretty_links_table WHERE link_id=".$link->link_id);
-
-    ?>
-      <span><strong><?php print $base_url . "/"; ?></strong></span><input type="text" name="link_pretty_link" value="<?php print $pretty_link; ?>" id="link_pretty_link" />
-      <p>This is the URL that will display for this link. When clicked, it will simply redirect to the main URL (Web Address) above. If it is left blank then the link will appear as it looks in the Web Address field on this page.</p>
-    <?php
-}
-
-add_action('admin_menu', 'prli_add_custom_box');
-//add_action('submitlink_box', 'prli_custom_box');
-add_action('add_link', 'prli_add_link_custom_box');
-add_action('edit_link', 'prli_edit_link_custom_box');
-*/
-
-/********* MODIFY BOOKMARK LIST ***********/
-/*
-function prli_bookmark_list($bookmarks_html)
-{
-  global $wpdb;
-  $base_url = get_option('siteurl');
-  $pretty_links_table = $wpdb->prefix . "prli_links";
-
-  $bookmark_query = "SELECT * FROM $pretty_links_table";
-  $pretty_links = $wpdb->get_results($bookmark_query);
-
-  foreach($pretty_links as $pretty_link)
-  {
-    if($pretty_link->pretty_link != null and $pretty_link->pretty_link != '')
-    {
-      $ugly_link = get_bookmark($pretty_link->link_id);
-      $bookmarks_html = preg_replace('#' . $ugly_link->link_url . '#', $base_url . '/' . $pretty_link->pretty_link, $bookmarks_html);
-    }
-  }
-
-  echo $bookmarks_html;
-}
-
-add_filter('wp_list_bookmarks', 'prli_bookmark_list');
-*/
-
-/********* ADD LINK COLUMNS ***********/
-/*
-function prli_link_custom_column($column_name, $id) {
-  global $wpdb;
-  $base_url = get_option('siteurl');
-  echo 'WORDDDD!';
-  $pretty_links_table = $wpdb->prefix . "prli_links";
-
-  if( $column_name == 'pretty_link' )
-  {
-    $query = "SELECT pretty_link FROM $pretty_link_table ".
-             "WHERE link_id='$id'";
-    $pretty_link = $base_url . "/" . $wpdb->get_var($query);
-    echo $pretty_link;
-  }
-  else
-  {
-    echo '<i>'.__('None').'</i>';
-  }
-}
-function prli_link_columns($defaults) {
-    echo $defaults;
-    $defaults['pretty_link'] = 'Pretty Link';
-    unset($defaults['name']);
-    return $defaults;
-}
-add_action('manage_link_custom_column','prli_link_custom_column', 2, 3);
-add_filter('manage_link_list','prli_link_columns');
-*/
 
 /********* ADD REDIRECTS YO ***********/
 function link_rewrite($wp_rewrite) {
