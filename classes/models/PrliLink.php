dae4f48acb00edd8b38efcd98f6b184462171e24
supercleanse <supercleanse@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-25 05:10:25 +0000

    1.3.13 Release of Pretty Link -- bug fixes
    git-svn-id: https://plugins.svn.wordpress.org/pretty-link/tags/1.3.13@113288 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/models/PrliLink.php
@@ -0,0 +1,214 @@
+<?php
+class PrliLink
+{
+    function table_name()
+    {
+      global $wpdb;
+      return $wpdb->prefix . 'prli_links';
+    }
+
+    function create( $values )
+    {
+      global $wpdb, $wp_rewrite;
+
+      $values['name'] = (!empty($values['name'])?$values['name']:$values['slug']);
+      $query = 'INSERT INTO ' . $this->table_name() . 
+               ' (url,slug,name,param_forwarding,param_struct,redirect_type,description,track_as_img,created_at) VALUES (\'' .
+                     $values['url'] . '\',\'' . 
+                     $values['slug'] . '\',\'' . 
+                     $values['name'] . '\',\'' . 
+                     $values['param_forwarding'] . '\',\'' . 
+                     $values['param_struct'] . '\',\'' . 
+                     $values['redirect_type'] . '\',\'' . 
+                     $values['description'] . '\',' . 
+                     (int)isset($values['track_as_img']) . ',' . 
+                     'NOW())';
+      $query_results = $wpdb->query($query);
+      $wp_rewrite->flush_rules();
+      return $query_results;
+    }
+
+    function update( $id, $values )
+    {
+      global $wpdb, $wp_rewrite;
+
+      $values['name'] = (!empty($values['name'])?$values['name']:$values['slug']);
+      $query = 'UPDATE ' . $this->table_name() . 
+                  ' SET url=\'' . $values['url'] . '\', ' .
+                      ' slug=\'' . $values['slug'] . '\', ' .
+                      ' name=\'' . $values['name'] . '\', ' .
+                      ' param_forwarding=\'' . $values['param_forwarding'] . '\', ' .
+                      ' param_struct=\'' . $values['param_struct'] . '\', ' .
+                      ' redirect_type=\'' . $values['redirect_type'] . '\', ' .
+                      ' description=\'' . $values['description'] . '\', ' .
+                      ' track_as_img=' . (int)isset($values['track_as_img']) .
+                  ' WHERE id='.$id;
+      $query_results = $wpdb->query($query);
+      $wp_rewrite->flush_rules();
+      return $query_results;
+    }
+
+    function destroy( $id )
+    {
+      require_once(PRLI_MODELS_PATH.'/models.inc.php');
+      global $wpdb, $prli_click, $wp_rewrite;
+
+      $reset = 'DELETE FROM ' . $prli_click->table_name() .  ' WHERE link_id=' . $id;
+      $destroy = 'DELETE FROM ' . $this->table_name() .  ' WHERE id=' . $id;
+
+      $wp_rewrite->flush_rules();
+
+      $wpdb->query($reset);
+      return $wpdb->query($destroy);
+    }
+
+    function reset( $id )
+    {
+      require_once(PRLI_MODELS_PATH.'/models.inc.php');
+      global $wpdb, $wp_rewrite, $prli_click;
+
+      $reset = 'DELETE FROM ' . $prli_click->table_name() .  ' WHERE link_id=' . $id;
+      return $wpdb->query($reset);
+    }
+
+    function getOneFromSlug( $slug )
+    {
+        global $wpdb;
+        $click_table = $wpdb->prefix . "prli_clicks";
+        $query = 'SELECT * FROM ' . $this->table_name() . ' WHERE slug=\'' . $slug . '\'';
+        return $wpdb->get_row($query);
+    }
+
+    function getOne( $id )
+    {
+        global $wpdb, $prli_click;
+        $click_table = $wpdb->prefix . "prli_clicks";
+        $query = 'SELECT li.*, (SELECT COUNT(*) FROM ' . $click_table . ' cl WHERE cl.link_id = li.id' . $prli_click->get_exclude_where_clause( ' AND' ) . ') as clicks FROM ' . $this->table_name() . ' li WHERE id=' . $id . ';';
+        return $wpdb->get_row($query);
+    }
+
+    function getAll()
+    {
+        global $wpdb, $prli_click;
+        $click_table = $wpdb->prefix . "prli_clicks";
+        $query = 'SELECT li.*, (SELECT COUNT(*) FROM ' . $click_table . ' cl WHERE cl.link_id = li.id' . $prli_click->get_exclude_where_clause( ' AND' ) . ') as clicks FROM ' . $this->table_name() . ' li;';
+        return $wpdb->get_results($query);
+    }
+
+    // Pagination Methods
+    function getRecordCount($where="")
+    {
+        global $wpdb;
+        $query = 'SELECT COUNT(*) FROM ' . $this->table_name() . $where;
+        return $wpdb->get_var($query);
+    }
+
+    function getPageCount($p_size, $where="")
+    {
+        return ceil((int)$this->getRecordCount($where) / (int)$p_size);
+    }
+
+    function getPage($current_p,$p_size, $where = "")
+    {
+        global $wpdb, $prli_click;
+        $click_table = $wpdb->prefix . "prli_clicks";
+        $end_index = $current_p * $p_size;
+        $start_index = $end_index - $p_size;
+        $query = 'SELECT li.*, (SELECT COUNT(*) FROM ' . $click_table . ' cl WHERE cl.link_id = li.id' . $prli_click->get_exclude_where_clause( ' AND' ) . ') as clicks FROM ' . $this->table_name() . ' li' . $where . ' LIMIT ' . $start_index . ',' . $p_size . ';';
+        $results = $wpdb->get_results($query);
+        return $results;
+    }
+
+    /** I'm generating a slug that is by default 2-3 characters long.
+      * This gives us a possibility of 36^3 - 37 = 46,619 possible
+      * random slugs. That should be *more* than enough slugs for
+      * any website -- if I get any feedback that we need more then
+      * I can always make a config option to raise the # of chars.
+      */
+    function generateValidSlug($num_chars = 3)
+    {
+      global $wpdb, $prli_utils;
+
+      // We're doing a base 36 hash which is why we're always doing everything by 36
+      $max_slug_value = pow(36,$num_chars);
+      $min_slug_value = 37; // we want to have at least 2 characters in the slug
+      $slug = base_convert( rand($min_slug_value,$max_slug_value), 10, 36 );
+
+      $query = "SELECT slug FROM " . $this->table_name(); // . " WHERE slug='" . $slug . "'";
+      $slugs = $wpdb->get_col($query,0);
+
+      // It is highly unlikely that we'll ever see 2 identical random slugs
+      // but just in case, here's some code to prevent collisions
+      while( in_array($slug,$slugs) or !$prli_utils->slugIsAvailable($slug) )
+        $slug = base_convert( rand($min_slug_value,$max_slug_value), 10, 36 );
+
+      return $slug;
+    }
+    
+    function get_pretty_link_url($slug)
+    {
+      global $prli_blogurl;
+
+      $link = $this->getOneFromSlug($slug);
+
+      if((isset($link->param_forwarding) and $link->param_forwarding == 'custom') and
+         (isset($link->track_as_img) and $link->track_as_img == 1))
+        return "&lt;img src=\"".$prli_blogurl . '/' . $link->slug . $link->param_struct . "\" width=\"1\" height=\"1\" style=\"display: none\" /&gt;";
+      else if((!isset($link->param_forwarding) or $link->param_forwarding != 'custom') and
+              (isset($link->track_as_img) and $link->track_as_img == 1))
+        return "&lt;img src=\"".$prli_blogurl . '/' . $link->slug . "\" width=\"1\" height=\"1\" style=\"display: none\" /&gt;";
+      else if((isset($link->param_forwarding) and $link->param_forwarding == 'custom') and
+              (!isset($link->track_as_img) or $link->track_as_img == 0))
+        return $prli_blogurl . '/' . $link->slug . $link->param_struct;
+      else
+        return $prli_blogurl . '/' . $link->slug;
+    }
+
+    function validate( $values )
+    {
+      global $wpdb, $prli_utils;
+
+      $errors = array();
+      if( ( $values['url'] == null or $values['url'] == '') and $values['track_as_img'] != 'on' )
+        $errors[] = "Target URL can't be blank -- unless this Pretty Link is being used as a tracking pixel (see Advanced Options on this page)";
+
+      if( $values['slug'] == null or $values['slug'] == '' )
+        $errors[] = "Pretty Link can't be blank";
+
+      if( !empty($values['url']) and !preg_match('/^http.?:\/\/.*\..*$/', $values['url'] ) )
+        $errors[] = "Link URL must be a correctly formatted url";
+
+      if( !preg_match('/^[a-zA-Z0-9\.\-_]+$/', $values['slug'] ) )
+        $errors[] = "Pretty Link must not contain spaces or special characters";
+
+      if($values['id'] != null and $values['id'] != '')
+        $query = "SELECT slug FROM " . $this->table_name() . " WHERE slug='" . $values['slug'] . "' AND id <> " . $values['id'];
+      else
+        $query = "SELECT slug FROM " . $this->table_name() . " WHERE slug='" . $values['slug'] . "'";
+
+      $slug_already_exists = $wpdb->get_var($query);
+
+      if( $slug_already_exists or !$prli_utils->slugIsAvailable($values['slug']) )
+        $errors[] = "This pretty link slug is already taken, please choose a different one";
+
+      if( isset($values['param_forwarding']) and $values['param_forwarding'] == 'custom' and empty($values['param_struct']) )
+        $errors[] = "If Custom Parameter Forwarding has been selected then you must specify a forwarding format.";
+
+      if( isset($values['param_forwarding']) and $values['param_forwarding'] == 'custom' and !preg_match('#%.*?%#', $values['param_struct']) )
+        $errors[] = "Your parameter forwarding must have at least one parameter specified in the format ex: <code>/%var1%/%var_two%/%varname3% ...</code>";
+
+      /*
+      if(isset($values['track_as_img']) and $values['track_as_img'] == 'on' and $values['url'] != null and $values['url'] != '')
+      {
+        $size = getimagesize($values['url']);
+        if(!preg_match('#image#',$size['mime']))
+        {
+          $errors[] = "If you want to track this pretty link as an image then your target url must be an image (png, jpeg, gif, etc.)";
+        }
+      }
+      */
+
+      return $errors;
+    }
+}
+?>
---
+++ classes/views/prli-clicks/list.php
@@ -0,0 +1,97 @@
+<div class="wrap">
+  <p style="font-size: 14px; font-weight: bold; float: right; padding-top: 25px;"><a href="http://blairwilliams.com/faq" target="_blank">Get Help</a>&nbsp;|&nbsp;<a href="http://blairwilliams.com/blog" target="_blank">Blog</a>&nbsp;|&nbsp;<a href="http://blairwilliams.com/don" target="_blank">Donate</a></p>
+  <h2><img src="<?php echo PRLI_URL.'/images/pretty-link-med.png'; ?>"/>&nbsp;Pretty Link: Hits</h2>
+  <br/>
+  <div id="search_pane" style="float: right;">
+    <form class="form-fields" name="click_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+      <?php wp_nonce_field('prli-clicks'); ?>
+
+      <input type="hidden" name="sort" id="sort" value="<?php echo $sort_str; ?>" />
+      <input type="hidden" name="sdir" id="sort" value="<?php echo $sdir_str; ?>" />
+      <input type="text" name="search" id="search" value="<?php echo $search_str; ?>" style="display:inline;"/>
+      <div class="submit" style="display: inline;"><input type="submit" name="Submit" value="Search"/>
+      <?php
+      if(!empty($search_str))
+      {
+      ?>
+      or <a href="?page=<?php echo PRLI_PLUGIN_NAME; ?>/prli-clicks.php<?php echo (isset($_GET['l'])?'&l='.$_GET['l']:''); ?>">Reset</a>
+      <?php
+      }
+      ?>
+      </div>
+    </form>
+  </div>
+  <h3>For <?php echo $link_name; ?></h3>
+
+<?php
+  if(isset($_GET['l']))
+    echo '<a href="?page='. PRLI_PLUGIN_NAME .'/prli-links.php">&laquo Back to Links</a>';
+  else if(isset($_GET['ip']) or isset($_GET['vuid']))
+    echo '<a href="?page='. PRLI_PLUGIN_NAME .'/prli-clicks.php">&laquo Back to Hits</a>';
+
+  require(PRLI_VIEWS_PATH.'/shared/table-nav.php');
+?>
+
+<table class="widefat post fixed" cellspacing="0">
+    <thead>
+    <tr>
+      <th class="manage-column" width="5%"><a href="?page=<?php echo PRLI_PLUGIN_NAME; ?>/prli-clicks.php&sort=btype<?php echo (($sort_str == 'btype' and $sdir_str == 'asc')?'&sdir=desc':''); ?>">Browser<?php echo (($sort_str == 'btype')?'&nbsp;&nbsp;&nbsp;<img src="'.$prli_siteurl.'/wp-content/plugins/'.PRLI_PLUGIN_NAME.'/images/'.(($sdir_str == 'desc')?'arrow_down.png':'arrow_up.png').'"/>':'') ?></a></th>
+      <th class="manage-column" width="12%"><a href="?page=<?php echo PRLI_PLUGIN_NAME; ?>/prli-clicks.php&sort=ip<?php echo (($sort_str == 'ip' and $sdir_str == 'asc')?'&sdir=desc':''); ?>">IP<?php echo (($sort_str == 'ip')?'&nbsp;&nbsp;&nbsp;<img src="'.$prli_siteurl.'/wp-content/plugins/'.PRLI_PLUGIN_NAME.'/images/'.(($sdir_str == 'desc')?'arrow_down.png':'arrow_up.png').'"/>':'') ?></a></th>
+      <th class="manage-column" width="12%"><a href="?page=<?php echo PRLI_PLUGIN_NAME; ?>/prli-clicks.php&sort=vuid<?php echo (($sort_str == 'vuid' and $sdir_str == 'asc')?'&sdir=desc':''); ?>">Visitor<?php echo (($sort_str == 'vuid')?'&nbsp;&nbsp;&nbsp;<img src="'.$prli_siteurl.'/wp-content/plugins/'.PRLI_PLUGIN_NAME.'/images/'.(($sdir_str == 'desc')?'arrow_down.png':'arrow_up.png').'"/>':'') ?></a></th>
+      <th class="manage-column" width="13%"><a href="?page=<?php echo PRLI_PLUGIN_NAME; ?>/prli-clicks.php&sort=created_at<?php echo (($sort_str == 'created_at' and $sdir_str == 'asc')?'&sdir=desc':''); ?>">Timestamp<?php echo ((empty($sort_str) or $sort_str == 'created_at')?'&nbsp;&nbsp;&nbsp;<img src="'.$prli_siteurl.'/wp-content/plugins/'.PRLI_PLUGIN_NAME.'/images/'.((empty($sort_str) or $sdir_str == 'desc')?'arrow_down.png':'arrow_up.png').'"/>':'') ?></a></th>
+      <th class="manage-column" width="16%"><a href="?page=<?php echo PRLI_PLUGIN_NAME; ?>/prli-clicks.php&sort=host<?php echo (($sort_str == 'host' and $sdir_str == 'asc')?'&sdir=desc':''); ?>">Host<?php echo (($sort_str == 'host')?'&nbsp;&nbsp;&nbsp;<img src="'.$prli_siteurl.'/wp-content/plugins/'.PRLI_PLUGIN_NAME.'/images/'.(($sdir_str == 'desc')?'arrow_down.png':'arrow_up.png').'"/>':'') ?></a></th>
+      <th class="manage-column" width="16%"><a href="?page=<?php echo PRLI_PLUGIN_NAME; ?>/prli-clicks.php&sort=uri<?php echo (($sort_str == 'uri' and $sdir_str == 'asc')?'&sdir=desc':''); ?>">URI<?php echo (($sort_str == 'uri')?'&nbsp;&nbsp;&nbsp;<img src="'.$prli_siteurl.'/wp-content/plugins/'.PRLI_PLUGIN_NAME.'/images/'.(($sdir_str == 'desc')?'arrow_down.png':'arrow_up.png').'"/>':'') ?></a></th>
+      <th class="manage-column" width="16%"><a href="?page=<?php echo PRLI_PLUGIN_NAME; ?>/prli-clicks.php&sort=referer<?php echo (($sort_str == 'referer' and $sdir_str == 'asc')?'&sdir=desc':''); ?>">Referrer<?php echo (($sort_str == 'referer')?'&nbsp;&nbsp;&nbsp;<img src="'.$prli_siteurl.'/wp-content/plugins/'.PRLI_PLUGIN_NAME.'/images/'.(($sdir_str == 'desc')?'arrow_down.png':'arrow_up.png').'"/>':'') ?></a></th>
+      <th class="manage-column" width="13%"><a href="?page=<?php echo PRLI_PLUGIN_NAME; ?>/prli-clicks.php&sort=link<?php echo (($sort_str == 'link' and $sdir_str == 'asc')?'&sdir=desc':''); ?>">Link<?php echo (($sort_str == 'link')?'&nbsp;&nbsp;&nbsp;<img src="'.$prli_siteurl.'/wp-content/plugins/'.PRLI_PLUGIN_NAME.'/images/'.(($sdir_str == 'desc')?'arrow_down.png':'arrow_up.png').'"/>':'') ?></a></th>
+    </tr>
+    </thead>
+  <?php
+
+  if(count($clicks) <= 0)
+  {
+      ?>
+    <tr>
+      <td colspan="7">No Hits have been recorded yet</td>
+    </tr>
+    <?php
+  }
+  else
+  {
+    foreach($clicks as $click)
+    {
+      ?>
+      <tr>
+        <td><img src="<?php echo $prli_siteurl; ?>/wp-content/plugins/<?php echo PRLI_PLUGIN_NAME; ?>/images/browser/<?php echo prli_browser_image($click->btype); ?>" alt="<?php echo $click->btype . " v" . $click->bversion; ?>" title="<?php echo $click->btype . " v" . $click->bversion; ?>"/>&nbsp;<img src="<?php echo $prli_siteurl; ?>/wp-content/plugins/<?php echo PRLI_PLUGIN_NAME; ?>/images/os/<?php echo prli_os_image($click->os); ?>" alt="<?php echo $click->os; ?>" title="<?php echo $click->os; ?>"/></td>
+        <td><a href="?page=<?php echo PRLI_PLUGIN_NAME; ?>/prli-clicks.php&ip=<?php echo $click->ip; ?>" title="View All Activity for IP Address: <?php echo $click->ip; ?>"><?php echo $click->ip; ?> (<?php echo $click->ip_count; ?>)</a></td>
+        <td><a href="?page=<?php echo PRLI_PLUGIN_NAME; ?>/prli-clicks.php&vuid=<?php echo $click->vuid; ?>" title="View All Activity for Visitor: <?php echo $click->vuid; ?>"><?php echo $click->vuid; ?><?php echo (($click->vuid != null)?" ($click->vuid_count)":''); ?></a></td>
+        <td><?php echo $click->created_at; ?></td>
+        <td><?php echo $click->host; ?></td>
+        <td><?php echo $click->uri; ?></td>
+        <td><?php echo $click->referer; ?></td>
+        <td><?php echo $click->link_name; ?></td>
+      </tr>
+      <?php
+    }
+  }
+  ?>
+    <tfoot>
+    <tr>
+      <th class="manage-column">Browser</th>
+      <th class="manage-column">IP</th>
+      <th class="manage-column">Visitor</th>
+      <th class="manage-column">Timestamp</th>
+      <th class="manage-column">Host</th>
+      <th class="manage-column">URI</th>
+      <th class="manage-column">Referrer</th>
+      <th class="manage-column">Link</th>
+    </tr>
+    </tfoot>
+</table>
+
+<a href="<?php echo $prli_siteurl; ?>/wp-content/plugins/<?php echo PRLI_PLUGIN_NAME; ?>/prli-clicks.php?action=csv<?php echo $page_params; ?>">Download CSV (<?php echo $link_name; ?>)</a>
+
+<?php
+  require(PRLI_VIEWS_PATH.'/shared/table-nav.php');
+?>
+
+</div>
