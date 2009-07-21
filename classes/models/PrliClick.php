3bcc703897bbc4f76990a4587cd98a64efa138e0
supercleanse <supercleanse@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-21 17:22:21 +0000

    1.3.41 Release of Pretty Link - This includes numerous bug fixes, back end hooks and code to interface with Pretty Link Pro
    git-svn-id: https://plugins.svn.wordpress.org/pretty-link/tags/1.3.41@137507 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/models/PrliClick.php
@@ -0,0 +1,241 @@
+<?php
+class PrliClick
+{
+    var $table_name;
+
+    function PrliClick()
+    {
+      global $wpdb;
+      $this->table_name = "{$wpdb->prefix}prli_clicks";
+    }
+
+    function get_ip_exclude_list()
+    {
+      $exclude_list = get_option('prli_exclude_ips');
+      $exclude_list = preg_replace('#[ \t]#','',$exclude_list);
+
+      if($exclude_list)
+        return "'" . implode("','", explode(',',$exclude_list)) . "'";
+      else
+        return '';
+    }
+
+    function get_exclude_where_clause( $where = '', $abbr = 'cl')
+    {
+      $exclude_list = $this->get_ip_exclude_list();
+
+      if($where == '')
+        $starts_with = '';
+      else
+        $starts_with = ' AND';
+      
+      if( $exclude_list != '')
+        return $starts_with . " $abbr.ip NOT IN (" . $exclude_list . ')';
+      else
+        return '';
+    }
+
+    function getOne( $id )
+    {
+        global $wpdb, $prli_link, $prli_utils;
+        $click_table = $wpdb->prefix . "prli_clicks";
+        $query = 'SELECT cl.*, (SELECT count(*) FROM '. $this->table_name .' cl2 WHERE cl2.ip = cl.ip) as ip_count, (SELECT count(*) FROM '. $this->table_name .' cl3 WHERE cl3.vuid = cl.vuid) as vuid_count, li.name as link_name FROM ' . $this->table_name . ' cl, ' . $prli_link->table_name . ' li WHERE li.id = cl.link_id AND id=' . $id . $prli_utils->prepend_and_or_where(' AND',$this->get_exclude_where_clause());
+    
+        return $wpdb->get_row($query);
+    }
+
+    // SELECT cl.*,li.name as link_name FROM wp_prli_clicks cl, wp_prli_links li WHERE li.id = cl.link_id ORDER BY created_at DESC
+    function getAll($where = '', $order = '')
+    {
+        global $wpdb, $prli_link, $prli_utils;
+        $click_table = $wpdb->prefix . "prli_clicks";
+        $where .= $this->get_exclude_where_clause( $where );
+        $where = $prli_utils->prepend_and_or_where(' AND', $where);
+        $query = 'SELECT cl.*, (SELECT count(*) FROM '. $this->table_name .' cl2 WHERE cl2.ip = cl.ip) as ip_count, (SELECT count(*) FROM '. $this->table_name .' cl3 WHERE cl3.vuid = cl.vuid) as vuid_count, li.name as link_name FROM ' . $this->table_name . ' cl, ' . $prli_link->table_name . ' li WHERE li.id = cl.link_id' . $where . $order;
+        return $wpdb->get_results($query);
+    }
+
+    // Delete all of the clicks from the database.
+    function clearAllClicks()
+    {
+      global $wpdb;
+      $query = "TRUNCATE TABLE " . $this->table_name;
+      return $wpdb->query($query);
+    }
+
+    // Pagination Methods
+    function getRecordCount($where='')
+    {
+        global $wpdb, $prli_link, $prli_utils;
+        $where .= $this->get_exclude_where_clause( $where );
+        $where = $prli_utils->prepend_and_or_where(' WHERE', $where);
+        $query = 'SELECT COUNT(*) FROM ' . $this->table_name . ' cl'. $where;
+        return $wpdb->get_var($query);
+    }
+
+    function getPageCount($p_size, $where='')
+    {
+        return ceil((int)$this->getRecordCount($where) / (int)$p_size);
+    }
+
+    function getPage($current_p,$p_size, $where = '', $order = '')
+    {
+        global $wpdb, $prli_link, $prli_utils;
+        $click_table = $wpdb->prefix . "prli_clicks";
+        $end_index = $current_p * $p_size;
+        $start_index = $end_index - $p_size;
+        $where .= $this->get_exclude_where_clause( $where );
+        $where = $prli_utils->prepend_and_or_where(' AND', $where);
+        $query = 'SELECT cl.*, (SELECT count(*) FROM '. $this->table_name .' cl2 WHERE cl2.ip = cl.ip) as ip_count, (SELECT count(*) FROM '. $this->table_name .' cl3 WHERE cl3.vuid = cl.vuid) as vuid_count, li.name as link_name FROM ' . $this->table_name . ' cl, ' . $prli_link->table_name . ' li WHERE li.id = cl.link_id' . $where . $order . ' LIMIT ' . $start_index . ',' . $p_size . ';';
+        $results = $wpdb->get_results($query);
+        return $results;
+    }
+
+    function generateUniqueVisitorId($num_chars = 6)
+    {
+      global $wpdb, $prli_utils;
+
+      // We're doing a base 36 hash which is why we're always doing everything by 36
+      $max_vuid_value = pow(36,$num_chars);
+      $min_vuid_value = 37; 
+      $vuid = base_convert( mt_rand($min_vuid_value,$max_vuid_value), 10, 36 );
+     
+      $query = "SELECT DISTINCT vuid FROM ".$this->table_name;
+      $vuids = $wpdb->get_col($query,0);
+     
+      // It is highly unlikely that we'll ever see 2 identical random vuids
+      // but just in case, here's some code to prevent collisions
+      while( in_array($vuid,$vuids) )
+        $vuid = base_convert( mt_rand($min_vuid_value,$max_vuid_value), 10, 36 );
+     
+      return $vuid;
+    }
+
+    function get_counts_by_days($start_timestamp, $end_timestamp, $link_id = "all", $type = "all", $group = '')
+    {
+      global $wpdb, $prli_link;
+
+      $query = "SELECT DATE(cl.created_at) as cldate,COUNT(*) as clcount FROM ".$this->table_name." cl WHERE cl.created_at BETWEEN '".date("Y-n-j",$start_timestamp)." 00:00:00' AND '".date("Y-n-j",$end_timestamp)." 23:59:59'".$search_where.$this->get_exclude_where_clause( ' AND' );
+
+      if($link_id != "all")
+        $query .= " AND link_id=$link_id";
+
+      if(!empty($group))
+        $query .= " AND link_id IN (SELECT id FROM " . $prli_link->table_name . " WHERE group_id=$group)";
+
+      if($type == "unique")
+        $query .= " AND first_click=1";
+
+      $query .= ' GROUP BY DATE(cl.created_at)';
+
+      $clicks_array = $wpdb->get_results($query);
+
+      $temp_array = array();
+      $counts_array = array();
+      $dates_array = array();
+
+      // Refactor Array for use later on
+      foreach($clicks_array as $c)
+        $temp_array[$c->cldate] = $c->clcount;
+
+      // Get the dates array
+      for($c = $start_timestamp; $c <= $end_timestamp; $c += 60*60*24)
+        $dates_array[] = date("Y-m-d",$c);
+
+      // Make sure counts array is in order and includes zero click days
+      foreach($dates_array as $date_str)
+      {
+        if(isset($temp_array[$date_str]))
+          $counts_array[$date_str] = $temp_array[$date_str];
+        else
+          $counts_array[$date_str] = 0;
+      }
+
+      return $counts_array;
+    }
+
+
+    function setupClickLineGraph($start_timestamp,$end_timestamp, $link_id = "all", $type = "all", $group = '')
+    {
+      global $wpdb, $prli_utils, $prli_link, $prli_group;
+
+      $dates_array = $this->get_counts_by_days($start_timestamp,$end_timestamp,$link_id,$type,$group);
+
+      $top_click_count = $prli_utils->getTopValue(array_values($dates_array));
+
+      if(!empty($group))
+        $link_slug = "group: '" . $wpdb->get_var("SELECT name FROM ".$prli_group->table_name." WHERE id=$group") . "'";
+      else if($link_id == "all")
+        $link_slug = "all links";
+      else
+        $link_slug = "'/".$wpdb->get_var("SELECT slug FROM ".$prli_link->table_name." WHERE id=$link_id") . "'";
+
+      if($type == "all")
+        $type_string = "All hits";
+      else
+        $type_string = "Unique hits";
+
+      $json_array = array(
+        "elements" => array( array( 
+          "type" => "line", 
+          "values" => array_values($dates_array),
+          "dot-style" => array( 
+            "type" => "dot",
+            "dot-size" => 4,
+            "colour" => "#ffc94e",
+            "halo-size" => 1,
+            "tip" => "#val# Hits<br>#x_label#"
+          ),
+          "width" => 2
+        ) ),
+        "title" => array(
+          "text" => 'Pretty Link: '.$type_string.' on '.$link_slug. ' between ' . date("Y-n-j",$start_timestamp) . ' and ' . date("Y-n-j",$end_timestamp),
+          "style" => "font-size: 16px; font-weight: bold; color: #3030d0; text-align: center; padding-bottom: 5px;"
+        ),
+        "bg_colour" => "-1",
+        "y_axis" => array(
+          "min" => 0,
+          "max" => $top_click_count,
+          "steps" => (int)(($top_click_count>=10)?$top_click_count/10:1),
+          "colour" => "#A2ACBA"
+        ),
+        "x_axis" => array(
+          "colour" => "#A2ACBA",
+          "grid-colour" => "#ffefa7",
+          "offset" => false,
+          "steps" => 4,
+          "labels" => array(
+            "steps" => 2,
+            "rotate" => 25,
+            "colour" => "#000000",
+            "labels" => array_keys($dates_array) 
+          )
+        )
+      );
+
+      return $prli_utils->prli_json_encode($json_array);
+    }
+
+
+    // Set defaults and grab get or post of each possible param
+    function get_params_array()
+    {
+      $values = array(
+         'paged'  => (isset($_GET['paged'])?$_GET['paged']:(isset($_POST['paged'])?$_POST['paged']:1)),
+         'l'      => (isset($_GET['l'])?$_GET['l']:(isset($_POST['l'])?$_POST['l']:'all')),
+         'group'  => (isset($_GET['group'])?$_GET['group']:(isset($_POST['group'])?$_POST['group']:'')),
+         'ip'     => (isset($_GET['ip'])?$_GET['ip']:(isset($_POST['ip'])?$_POST['ip']:'')),
+         'vuid'   => (isset($_GET['vuid'])?$_GET['vuid']:(isset($_POST['vuid'])?$_POST['vuid']:'')),
+         'sdate'  => (isset($_GET['sdate'])?$_GET['sdate']:(isset($_POST['sdate'])?$_POST['sdate']:'')),
+         'edate'  => (isset($_GET['edate'])?$_GET['edate']:(isset($_POST['edate'])?$_POST['edate']:'')),
+         'type'   => (isset($_GET['type'])?$_GET['type']:(isset($_POST['type'])?$_POST['type']:'all')),
+         'search' => (isset($_GET['search'])?$_GET['search']:(isset($_POST['search'])?$_POST['search']:'')),
+         'sort'   => (isset($_GET['sort'])?$_GET['sort']:(isset($_POST['sort'])?$_POST['sort']:'')),
+         'sdir'   => (isset($_GET['sdir'])?$_GET['sdir']:(isset($_POST['sdir'])?$_POST['sdir']:''))
+      );
+
+      return $values;
+    }
+
+}
+?>
---
+++ prli-options.php
@@ -0,0 +1,248 @@
+<?php
+require_once 'prli-config.php';
+require_once(PRLI_MODELS_PATH . '/models.inc.php');
+
+$errors = array();
+
+if($_GET['action'] == 'pro-settings' or $_POST['action'] == 'pro-settings' or
+   $_GET['action'] == 'force-pro-reinstall' or $_POST['action'] == 'force-pro-reinstall' or
+   $_GET['action'] == 'pro-uninstall' or $_POST['action'] == 'pro-uninstall')
+{
+  // variables for the field and option names 
+  $prlipro_username = 'prlipro_username';
+  $prlipro_password = 'prlipro_password';
+  $hidden_field_name = 'prli_update_options';
+  
+  $prli_domain = "pretty-link";
+  
+  // Read in existing option value from database
+  $prlipro_username_val = get_option( $prlipro_username );
+  $prlipro_password_val = get_option( $prlipro_password );
+  
+  if($_GET['action'] == 'force-pro-reinstall')
+  {
+    $prli_utils->download_and_install_pro($prlipro_username_val, $prlipro_password_val, true);
+    ?>
+    
+    <div class="updated"><p><strong><?php _e('Pretty Link Pro Successfully Reinstalled.', $prli_domain ); ?></strong></p></div>
+    <?php
+  }
+  if($_GET['action'] == 'pro-uninstall')
+  {
+    $prli_utils->uninstall_pro();
+    ?>
+    
+    <div class="updated"><p><strong><?php _e('Pretty Link Pro Successfully Uninstalled.', $prli_domain ); ?></strong></p></div>
+    <?php
+  }
+  else
+  {
+    // See if the user has posted us some information
+    // If they did, this hidden field will be set to 'Y'
+    if( $_POST[ $hidden_field_name ] == 'Y' ) 
+    {
+      // Validate This
+      // This is where the remote username / password will be validated
+      //if( !empty($_POST[$prettybar_image_url]) and !preg_match('/^http.?:\/\/.*\..*$/', $_POST[$prettybar_image_url] ) )
+      //  $errors[] = "Logo Image URL must be a correctly formatted URL";
+    
+      // Read their posted value
+      $prlipro_username_val = stripslashes($_POST[ $prlipro_username ]);
+      $prlipro_password_val = stripslashes($_POST[ $prlipro_password ]);
+    
+      if( count($errors) > 0 )
+      {
+        require(PRLI_VIEWS_PATH.'/shared/errors.php');
+      }
+      else
+      {
+        // TODO: Download & Install Pretty Link Pro if Account is valid and software isn't currently installed
+        // Save the posted value in the database
+        update_option( $prlipro_username, $prlipro_username_val );
+        update_option( $prlipro_password, $prlipro_password_val );
+    
+        // Put an options updated message on the screen
+        $message = $prli_utils->download_and_install_pro($prlipro_username_val, $prlipro_password_val, true);
+  
+        $message = (($message == 'SUCCESS')?'Pretty Link Pro has been installed click here to get started: <a href="?page=pretty-link/pro/prlipro-options.php">Pretty Link Pro Options</a>':$message);
+    ?>
+    
+    <div class="updated"><p><strong><?php _e($message, $prli_domain ); ?></strong></p></div>
+    <?php
+      }
+    }
+  }
+  
+  require_once 'classes/views/prli-options/pro-settings.php';
+  
+}
+else
+{
+  // variables for the field and option names 
+  $prli_exclude_ips  = 'prli_exclude_ips';
+  $prettybar_image_url  = 'prli_prettybar_image_url';
+  $prettybar_background_image_url  = 'prli_prettybar_background_image_url';
+  $prettybar_color  = 'prli_prettybar_color';
+  $prettybar_text_color  = 'prli_prettybar_text_color';
+  $prettybar_link_color  = 'prli_prettybar_link_color';
+  $prettybar_hover_color  = 'prli_prettybar_hover_color';
+  $prettybar_visited_color  = 'prli_prettybar_visited_color';
+  $prettybar_show_title  = 'prli_prettybar_show_title';
+  $prettybar_show_description  = 'prli_prettybar_show_description';
+  $prettybar_show_share_links  = 'prli_prettybar_show_share_links';
+  $prettybar_show_target_url_link  = 'prli_prettybar_show_target_url_link';
+  $prettybar_title_limit = 'prli_prettybar_title_limit';
+  $prettybar_desc_limit = 'prli_prettybar_desc_limit';
+  $prettybar_link_limit = 'prli_prettybar_link_limit';
+  $link_show_prettybar = 'prli_link_show_prettybar';
+  $link_ultra_cloak = 'prli_link_ultra_cloak';
+  $link_track_me = 'prli_link_track_me';
+  $link_track_as_pixel = 'prli_link_track_as_pixel';
+  $link_nofollow = 'prli_link_nofollow';
+  $link_redirect_type = 'prli_link_redirect_type';
+  $hidden_field_name = 'prli_update_options';
+  
+  $prli_domain = "pretty-link";
+  
+  // Read in existing option value from database
+  $prli_exclude_ips_val = get_option( $prli_exclude_ips );
+  $prettybar_image_url_val = get_option( $prettybar_image_url );
+  $prettybar_background_image_url_val = get_option( $prettybar_background_image_url );
+  $prettybar_color_val = get_option( $prettybar_color );
+  $prettybar_text_color_val = get_option( $prettybar_text_color );
+  $prettybar_link_color_val = get_option( $prettybar_link_color );
+  $prettybar_hover_color_val = get_option( $prettybar_hover_color );
+  $prettybar_visited_color_val = get_option( $prettybar_visited_color );
+  $prettybar_show_title_val = get_option( $prettybar_show_title );
+  $prettybar_show_description_val = get_option( $prettybar_show_description );
+  $prettybar_show_share_links_val = get_option( $prettybar_show_share_links );
+  $prettybar_show_target_url_link_val = get_option( $prettybar_show_target_url_link );
+  $prettybar_title_limit_val = get_option( $prettybar_title_limit );
+  $prettybar_desc_limit_val = get_option( $prettybar_desc_limit );
+  $prettybar_link_limit_val = get_option( $prettybar_link_limit );
+  $link_show_prettybar_val = get_option( $link_show_prettybar );
+  $link_ultra_cloak_val = get_option( $link_ultra_cloak );
+  $link_track_me_val = get_option( $link_track_me );
+  $link_track_as_pixel_val = get_option( $link_track_as_pixel );
+  $link_nofollow_val = get_option( $link_nofollow );
+  $link_redirect_type_val = get_option( $link_redirect_type );
+  
+  // See if the user has posted us some information
+  // If they did, this hidden field will be set to 'Y'
+  if( $_POST[ $hidden_field_name ] == 'Y' ) 
+  {
+    // Validate This
+    if( !empty($_POST[$prettybar_image_url]) and !preg_match('/^http.?:\/\/.*\..*$/', $_POST[$prettybar_image_url] ) )
+      $errors[] = "Logo Image URL must be a correctly formatted URL";
+  
+    if( !empty($_POST[$prettybar_background_image_url]) and !preg_match('/^http.?:\/\/.*\..*$/', $_POST[$prettybar_background_image_url] ) )
+      $errors[] = "Background Image URL must be a correctly formatted URL";
+  
+    if( !empty($_POST[ $prli_exclude_ips ]) and !preg_match( "#^[ \t]*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})([ \t]*,[ \t]*\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})*$#", $_POST[ $prli_exclude_ips ] ) )
+      $errors[] = "Must be a comma separated list of IP addresses.";
+  
+    if( !empty($_POST[ $prettybar_color ]) and !preg_match( "#^[0-9a-fA-F]{6}$#", $_POST[ $prettybar_color ] ) )
+      $errors[] = "PrettyBar Background Color must be an actual RGB Value";
+  
+    if( !empty($_POST[ $prettybar_text_color ]) and !preg_match( "#^[0-9a-fA-F]{6}$#", $_POST[ $prettybar_text_color ] ) )
+      $errors[] = "PrettyBar Text Color must be an actual RGB Value";
+  
+    if( !empty($_POST[ $prettybar_link_color ]) and !preg_match( "#^[0-9a-fA-F]{6}$#", $_POST[ $prettybar_link_color ] ) )
+      $errors[] = "PrettyBar Link Color must be an actual RGB Value";
+  
+    if( !empty($_POST[ $prettybar_hover_color ]) and !preg_match( "#^[0-9a-fA-F]{6}$#", $_POST[ $prettybar_hover_color ] ) )
+      $errors[] = "PrettyBar Hover Color must be an actual RGB Value";
+  
+    if( !empty($_POST[ $prettybar_visited_color ]) and !preg_match( "#^[0-9a-fA-F]{6}$#", $_POST[ $prettybar_visited_color ] ) )
+      $errors[] = "PrettyBar Hover Color must be an actual RGB Value";
+  
+    if( empty($_POST[ $prettybar_title_limit ]) )
+      $errors[] = "PrettyBar Title Character Limit must not be blank";
+  
+    if( empty($_POST[ $prettybar_desc_limit ]) )
+      $errors[] = "PrettyBar Description Character Limit must not be blank";
+  
+    if( empty($_POST[ $prettybar_link_limit ]) )
+      $errors[] = "PrettyBar Link Character Limit must not be blank";
+  
+    if( !empty($_POST[ $prettybar_title_limit ]) and !preg_match( "#^[0-9]*$#", $_POST[ $prettybar_title_limit ] ) )
+      $errors[] = "PrettyBar Title Character Limit must be a number";
+  
+    if( !empty($_POST[ $prettybar_desc_limit ]) and !preg_match( "#^[0-9]*$#", $_POST[ $prettybar_desc_limit ] ) )
+      $errors[] = "PrettyBar Description Character Limit must be a number";
+  
+    if( !empty($_POST[ $prettybar_link_limit ]) and !preg_match( "#^[0-9]*$#", $_POST[ $prettybar_link_limit ] ) )
+      $errors[] = "PrettyBar Link Character Limit must be a number";
+  
+    // Read their posted value
+    $prli_exclude_ips_val = stripslashes($_POST[ $prli_exclude_ips ]);
+    $prettybar_image_url_val = stripslashes($_POST[ $prettybar_image_url ]);
+    $prettybar_background_image_url_val = stripslashes($_POST[ $prettybar_background_image_url ]);
+    $prettybar_color_val = stripslashes($_POST[ $prettybar_color ]);
+    $prettybar_text_color_val = stripslashes($_POST[ $prettybar_text_color ]);
+    $prettybar_link_color_val = stripslashes($_POST[ $prettybar_link_color ]);
+    $prettybar_hover_color_val = stripslashes($_POST[ $prettybar_hover_color ]);
+    $prettybar_visited_color_val = stripslashes($_POST[ $prettybar_visited_color ]);
+    $prettybar_show_title_val = (int)isset($_POST[ $prettybar_show_title ]);
+    $prettybar_show_description_val = (int)isset($_POST[ $prettybar_show_description ]);
+    $prettybar_show_share_links_val = (int)isset($_POST[ $prettybar_show_share_links ]);
+    $prettybar_show_target_url_link_val = (int)isset($_POST[ $prettybar_show_target_url_link ]);
+    $prettybar_title_limit_val = stripslashes($_POST[ $prettybar_title_limit ]);
+    $prettybar_desc_limit_val = stripslashes($_POST[ $prettybar_desc_limit ]);
+    $prettybar_link_limit_val = stripslashes($_POST[ $prettybar_link_limit ]);
+    $link_show_prettybar_val = (int)isset($_POST[ $link_show_prettybar ]);
+    $link_ultra_cloak_val = (int)isset($_POST[ $link_ultra_cloak ]);
+    $link_track_me_val = (int)isset($_POST[ $link_track_me ]);
+    $link_track_as_pixel_val = (int)isset($_POST[ $link_track_as_pixel ]);
+    $link_nofollow_val = (int)isset($_POST[ $link_nofollow ]);
+    $link_redirect_type_val = $_POST[ $link_redirect_type ];
+  
+    if( count($errors) > 0 )
+    {
+      require(PRLI_VIEWS_PATH.'/shared/errors.php');
+    }
+    else
+    {
+      // Save the posted value in the database
+      update_option( $prli_exclude_ips, $prli_exclude_ips_val );
+      update_option( $prettybar_image_url, $prettybar_image_url_val );
+      update_option( $prettybar_background_image_url, $prettybar_background_image_url_val );
+      update_option( $prettybar_color, $prettybar_color_val );
+      update_option( $prettybar_text_color, $prettybar_text_color_val );
+      update_option( $prettybar_link_color, $prettybar_link_color_val );
+      update_option( $prettybar_hover_color, $prettybar_hover_color_val );
+      update_option( $prettybar_visited_color, $prettybar_visited_color_val );
+      update_option( $prettybar_show_title, $prettybar_show_title_val );
+      update_option( $prettybar_show_description, $prettybar_show_description_val );
+      update_option( $prettybar_show_share_links, $prettybar_show_share_links_val );
+      update_option( $prettybar_show_target_url_link, $prettybar_show_target_url_link_val );
+      update_option( $prettybar_title_limit, $prettybar_title_limit_val );
+      update_option( $prettybar_desc_limit, $prettybar_desc_limit_val );
+      update_option( $prettybar_link_limit, $prettybar_link_limit_val );
+      update_option( $link_show_prettybar, $link_show_prettybar_val );
+      update_option( $link_ultra_cloak, $link_ultra_cloak_val );
+      update_option( $link_track_me, $link_track_me_val );
+      update_option( $link_track_as_pixel, $link_track_as_pixel_val );
+      update_option( $link_nofollow, $link_nofollow_val );
+      update_option( $link_redirect_type, $link_redirect_type_val );
+  
+      // Put an options updated message on the screen
+  ?>
+  
+  <div class="updated"><p><strong><?php _e('Options saved.', $prli_domain ); ?></strong></p></div>
+  <?php
+    }
+  }
+  else if($_GET['action'] == 'clear_all_clicks4134' or $_POST['action'] == 'clear_all_clicks4134')
+  {
+    $prli_click->clearAllClicks();
+  ?>
+  
+  <div class="updated"><p><strong><?php _e('Hit Database Was Cleared.', $prli_domain ); ?></strong></p></div>
+  <?php
+  }
+  
+  require_once 'classes/views/prli-options/form.php';
+}
+
+?>
