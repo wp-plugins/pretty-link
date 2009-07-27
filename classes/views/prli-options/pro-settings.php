b04c7739938e1dc0a45940a07e82a36b34c8d97f
supercleanse <supercleanse@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-27 06:41:52 +0000

    More Pretty Link Pro integration...
    git-svn-id: https://plugins.svn.wordpress.org/pretty-link/trunk@139552 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/views/prli-options/pro-settings.php
+++ classes/views/prli-options/pro-settings.php
@@ -8,6 +8,9 @@
 <?php wp_nonce_field('update-options'); ?>
 
 <h3>Pretty Link Pro Account Information</h3>
+<?php if($prli_utils->pro_is_installed()) { ?>
+  <p><a href="http://prettylinkpro.com/user-manual">User Manual</a>&nbsp;|&nbsp;<a href="http://prettylinkpro.com/forum/">Support Forums</a></p>
+<?php } ?>
 <table class="form-table">
   <tr class="form-field">
     <td valign="top" width="15%"><?php _e("Pretty Link Pro Username*:", $prlipro_username ); ?> </td>
@@ -30,7 +33,8 @@
 </p>
 
 <?php if($prli_utils->pro_is_installed()) { ?>
-  <div><p><strong>The <?php echo $prli_utils->get_pro_version(); ?> Version of Pretty LInk Pro is Installed</strong></p><p><a href="<?php echo $this_uri; ?>&action=force-pro-reinstall" title="Re-Install">Re-Install</a>&nbsp;|&nbsp;<a href="<?php echo $this_uri; ?>&action=pro-uninstall" onclick="return confirm('Are you sure you want to Un-Install Pretty Link Pro? This will delete your pro username & password from your local database, remove all the pro software but will leave all your data intact incase you want to reinstall sometime :) ...');" title="Un-Install" >Un-Install</a></div>
+  <div><p><strong>The <?php echo $prli_utils->get_pro_version(); ?> Version of Pretty LInk Pro is Installed</strong></p><p><a href="<?php echo $this_uri; ?>&action=force-pro-reinstall" title="Re-Install">Re-Install</a>&nbsp;|&nbsp;<a href="<?php echo $this_uri; ?>&action=pro-uninstall" onclick="return confirm('Are you sure you want to Un-Install Pretty Link Pro? This will delete your pro username & password from your local database, remove all the pro software but will leave all your data intact incase you want to reinstall sometime :) ...');" title="Un-Install" >Un-Install</a></p><br/><p><strong>Edit/Update Your Profile:</strong><br/><span class="description">Use your account username and password to log in to your Account and Affiliate Control Panel</span></p><p><a href="http://prettylinkpro.com/amember/member.php">Account</a>&nbsp;|&nbsp;<a href="http://prettylinkpro.com/amember/aff_member.php">Affiliate Control Panel</a></div>
+  
 <?php } else { ?>
   <a href="http://prettylinkpro.com">Upgrade to Pretty Link Pro</a>
 <?php } ?>
