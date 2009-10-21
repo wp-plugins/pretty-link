f495d20ea92a4bf022c780d61b80c018a2eacf29
supercleanse <supercleanse@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-21 00:14:40 +0000

    Added some more support options and fixed some minor bugs in keyword replacement and pro options.
    git-svn-id: https://plugins.svn.wordpress.org/pretty-link/trunk@165318 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/views/shared/nav.php
+++ classes/views/shared/nav.php
@@ -1,1 +1,18 @@
-<p style="font-size: 14px; font-weight: bold; float: right; text-align: right; padding-top: 5px;"><span style="font-size: 12px;"><a href="http://blairwilliams.com/blog">Get the most from WordPress at BlairWilliams.com</a></span><br/><a href="http://blairwilliams.com/faq" target="_blank">Get Help</a>&nbsp;|&nbsp;<a href="http://blairwilliams.com/category/tutorial" target="_blank">Tutorials</a>&nbsp;|&nbsp;<a href="http://blairwilliams.com/don" target="_blank">Donate</a></p>
+<?php
+  global $prli_utils;
+  
+  if(!$prli_utils->pro_is_installed())
+  {
+    $support_link =<<<SUPPORT_LINK
+&nbsp;|&nbsp;<a href="http://prettylinkpro.com/user-manual">Pro Manual</a>&nbsp;|&nbsp;<a href="http://prettylinkpro.com/forum">Pro Forums</a>
+SUPPORT_LINK;
+  }
+  else
+  {
+    $support_link =<<<SUPPORT_LINK
+&nbsp;|&nbsp;<a href="http://prettylinkpro.com">Premium Support</a>
+SUPPORT_LINK;
+  }
+    
+?>
+<p style="font-size: 14px; font-weight: bold; float: right; text-align: right; padding-top: 5px;">Become a <a href="http://www.facebook.com/pages/Pretty-Link/283252860401">Fan of Pretty Link</a> on Facebook.<br/>Get Help:&nbsp;&nbsp;<a href="http://blairwilliams.com/category/tutorial" target="_blank">Tutorials</a>&nbsp;|&nbsp;<a href="http://twitter.com/blairwilli" target="_blank">Twitter</a><?php echo $support_link; ?></p>
