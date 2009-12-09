a39851e66f3ac9edc10dabb073e1169404aacb35
supercleanse <supercleanse@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-09 00:18:42 +0000

    wa hord
    git-svn-id: https://plugins.svn.wordpress.org/pretty-link/trunk@181089 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- prli-pro-settings.php
+++ prli-pro-settings.php
@@ -19,7 +19,7 @@ if($_GET['action'] == 'force-pro-reinstall')
   $prli_update->queue_update(true);
   $reinstall_url = wp_nonce_url('update.php?action=upgrade-plugin&plugin=pretty-link/pretty-link.php', 'upgrade-plugin_pretty-link/pretty-link.php');
 ?>
-<div class="updated"><p><strong><?php printf(__('You\'re almost done!<br/>Finish your %1$sRe-Install of Pretty Link Pro%2$s'), '<a href="'.$reinstall_url.'">', '</a>'); ?></strong></p></div>
+<div class="updated"><p><strong><?php printf(__('You\'re almost done!<br/>%1$sFinish your Re-Install of Pretty Link Pro%2$s'), '<a href="'.$reinstall_url.'">', '</a>'); ?></strong></p></div>
 <?php
 }
 if($_GET['action'] == 'pro-uninstall')
