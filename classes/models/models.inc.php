f8c1724bff2c5043b17fc945ad8d8c7a2fdee702
supercleanse <supercleanse@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-16 06:13:23 +0000

    Fixed dashboard performance issue..
    git-svn-id: https://plugins.svn.wordpress.org/pretty-link/trunk@263881 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/models/models.inc.php
+++ classes/models/models.inc.php
@@ -23,24 +23,39 @@ $prli_utils     = new PrliUtils();
 $prli_url_utils = new PrliUrlUtils();
 $prli_update    = new PrliUpdate();
 
-function prli_get_main_message( $message = "Get started by <a href=\"?page=pretty-link/prli-links.php&action=new\">adding a URL</a> that you want to turn into a pretty link.<br/>Come back to see how many times it was clicked.")
+function prli_get_main_message( $message = "Get started by <a href=\"?page=pretty-link/prli-links.php&action=new\">adding a URL</a> that you want to turn into a pretty link.<br/>Come back to see how many times it was clicked.", $expiration=3600) // Get new messages every 1 hour
 {
-  global $prli_update;
+  global $prli_update, $wp_version;
   include_once(ABSPATH."/wp-includes/class-IXR.php");
 
-  if($prli_update->pro_is_installed_and_authorized())
-  {
-    $client = new IXR_Client('http://prettylinkpro.com/xmlrpc.php');
-    if ($client->query('prlipro.get_main_message'))
-      $message = $client->getResponse();
-  }
+  $message_mothership = (($prli_update->pro_is_installed_and_authorized())?'http://prettylinkpro.com/xmlrpc.php':'http://blairwilliams.com/xmlrpc.php');
+
+  if( version_compare($wp_version, '3.0', '>=') )
+    $messages = get_site_transient('_prli_messages'); // for WordPress 3.0
   else
+    $messages = get_transient('_prli_messages'); // for WordPress 2.8+
+
+  // if the messages array has expired go back to the mothership
+  if($messages === false)
   {
-    $client = new IXR_Client('http://blairwilliams.com/xmlrpc.php');
-    if ($client->query('prli.get_main_message'))
-      $message = $client->getResponse();
+    $client = new IXR_Client($message_mothership);
+    if ($client->query('prli.get_main_message_array'))
+      $messages = $client->getResponse();
+
+    // If we're having connection issues on the mothership then store the default message in the transient
+    if(empty($messages) or !$messages or !is_array($messages))
+      $messages = array($message);
+
+    if( version_compare($wp_version, '3.0', '>=') )
+      set_site_transient("_prli_messages", $messages, $expiration); // for WordPress 3.0
+    else
+      set_transient("_prli_messages", $messages, $expiration); // for WordPress 2.8+
   }
-  return $message;
+
+  if(empty($messages) or !$messages or !is_array($messages))
+    return $message;
+  else
+    return $messages[array_rand($messages)];
 }
 
 ?>
