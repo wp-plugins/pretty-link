87c39923563e21015c7861a78f7e3164620707f3
supercleanse <supercleanse@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-12 04:10:17 +0000

    Minor change...
    git-svn-id: https://plugins.svn.wordpress.org/pretty-link/tags/1.1.8@274666 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pretty-link.php
+++ pretty-link.php
@@ -6,7 +6,7 @@ Description: Create clean, simple, trackable links on your website that redirect
 Version: 1.1.8
 Author: Blair Williams
 Author URI: http://blairwilliams.com
-Copyright: 2009, Blair Williams
+Copyright: 2009, Caseproof, LLC
 
 GNU General Public License, Free Software Foundation <http://creativecommons.org/licenses/GPL/2.0/>
 This program is free software; you can redistribute it and/or modify
