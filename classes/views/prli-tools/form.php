5d9dd16b0a1270f9255228d902819d6d8093842b
supercleanse <supercleanse@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-04 05:38:41 +0000

    Enhanced the tools page...
    git-svn-id: https://plugins.svn.wordpress.org/pretty-link/trunk@328503 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/views/prli-tools/form.php
+++ classes/views/prli-tools/form.php
@@ -1,27 +1,44 @@
 <div class="wrap">
+<script type="text/javascript">
+function toggle_tweetdeck_instructions()
+{
+  jQuery('.tweetdeck_instructions').slideToggle();
+}
+
+function toggle_iphone_instructions()
+{
+  jQuery('.iphone_instructions').slideToggle();
+}
+
+</script>
 <?php
   require(PRLI_VIEWS_PATH.'/shared/nav.php');
 ?>
   <h2><img src="<?php echo PRLI_URL.'/images/pretty-link-med.png'; ?>"/>&nbsp;Pretty Link: Tools</h2>
   <h3><?php _e('End-Point URL:'); ?></h3>
   <p><span class="description"><?php _e('This can be used to integrate with your twitter client.'); ?></span>
-  <pre><?php echo $prli_blogurl; ?>/index.php?action=prli_endpoint_url&k=<?php echo $prli_options->bookmarklet_auth; ?>&url=</pre>
-  <h4><?php _e('TweetDeck Integration:'); ?></h4>
-  <span class="description"><?php _e('Follow the'); ?> <a href="http://support.tweetdeck.com/entries/132632-add-a-custom-url-shortener" target="_blank">TweetDeck Custom URL Instructions</a><?php _e(' and add the following URL to TweetDeck'); ?></span>
-  <pre><?php echo $prli_blogurl; ?>/index.php?action=prli_endpoint_url&k=<?php echo $prli_options->bookmarklet_auth; ?>&url=%@</pre>
+    <pre><?php echo $prli_blogurl; ?>/index.php?action=prli_endpoint_url&k=<?php echo $prli_options->bookmarklet_auth; ?>&url=</pre>
+    <br/>
+    <a href="javascript:toggle_tweetdeck_instructions();"><strong><?php _e('Show TweetDeck Integration Instructions'); ?></strong></a>
+    <div class="tweetdeck_instructions" style="display: none">
+      <span class="description"><?php _e('Follow the'); ?> <a href="http://support.tweetdeck.com/entries/132632-add-a-custom-url-shortener" target="_blank"><?php _e('TweetDeck Custom URL Instructions'); ?></a><?php _e(' and add the following URL to TweetDeck'); ?></span>
+      <pre><?php echo $prli_blogurl; ?>/index.php?action=prli_endpoint_url&k=<?php echo $prli_options->bookmarklet_auth; ?>&url=%@</pre>
+    </div>
   </p>
   <h3>Bookmarklet: </h3>
   <p><strong><a href="javascript:location.href='<?php echo PRLI_URL; ?>/prli-bookmarklet.php?k=<?php echo $prli_options->bookmarklet_auth; ?>&target_url='+escape(location.href);">Get PrettyLink</a></strong><br/>
   <span class="description">Just drag this "Get PrettyLink" link to your toolbar to install the bookmarklet. As you browse the web, you can just click this bookmarklet to create a pretty link from the current url you're looking at.&nbsp;&nbsp;<a href="http://blairwilliams.com/pretty-link-bookmarklet/">(more help)</a></span>
-  <p><strong>Note:</strong> iPhone users can install this bookmarklet in their Safari to create Pretty Links with the following steps:<br/>
-  <ol>
-    <li>Copy this text:<br/><code>javascript:location.href='<?php echo PRLI_URL; ?>/prli-bookmarklet.php?k=<?php echo $prli_options->bookmarklet_auth; ?>&target_url='+escape(location.href);</code></li>
-    <li>Tap the + button at the bottom of the screen</li>
-    <li>Choose "Add Bookmark", rename your bookmark to "Get PrettyLink" (or whatever you want) and then "Save"</li>
-    <li>Navigate through your Bookmarks folders until you find the new bookmark and click "Edit"</li>
-    <li>Delete all the text from the address</li>
-    <li>Paste the text you copied in Step 1 into the address field</li>
-    <li>To save the changes hit "Bookmarks" and <strong>you're done!</strong> Now when you find a page you want to save off as a Pretty Link, just click the "Bookmarks" icon at the bottom of the screen and select your link.</li>
-  </ol>
+  <br/><br/><a href="javascript:toggle_iphone_instructions()"><strong><?php _e('Show iPhone Bookmarklet Instructions'); ?></strong></a>
+  <div class="iphone_instructions" style="display: none"><strong>Note:</strong> iPhone users can install this bookmarklet in their Safari to create Pretty Links with the following steps:<br/>
+    <ol>
+      <li>Copy this text:<br/><code>javascript:location.href='<?php echo PRLI_URL; ?>/prli-bookmarklet.php?k=<?php echo $prli_options->bookmarklet_auth; ?>&target_url='+escape(location.href);</code></li>
+      <li>Tap the + button at the bottom of the screen</li>
+      <li>Choose "Add Bookmark", rename your bookmark to "Get PrettyLink" (or whatever you want) and then "Save"</li>
+      <li>Navigate through your Bookmarks folders until you find the new bookmark and click "Edit"</li>
+      <li>Delete all the text from the address</li>
+      <li>Paste the text you copied in Step 1 into the address field</li>
+      <li>To save the changes hit "Bookmarks" and <strong>you're done!</strong> Now when you find a page you want to save off as a Pretty Link, just click the "Bookmarks" icon at the bottom of the screen and select your link.</li>
+    </ol>
+  </div>
 <?php do_action('prli-add-tools'); ?>
 </div>
