af71c9e09ae9dfd0f701269a859360964fde3672
supercleanse <supercleanse@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-04 17:49:07 +0000

    Altered the nav bar...
    git-svn-id: https://plugins.svn.wordpress.org/pretty-link/trunk@328738 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/views/shared/nav.php
+++ classes/views/shared/nav.php
@@ -2,17 +2,8 @@
   global $prli_update;
   
   if($prli_update->pro_is_installed_and_authorized())
-  {
-    $support_link =<<<SUPPORT_LINK
-&nbsp;|&nbsp;<a href="http://prettylinkpro.com/user-manual">Pro Manual</a>&nbsp;|&nbsp;<a href="http://prettylinkpro.com/forum">Pro Forums</a>
-SUPPORT_LINK;
-  }
+    $support_link = "&nbsp;|&nbsp;<a href=\"http://prettylinkpro.com/user-manual\">" . __('Pro Manual') . '</a>';
   else
-  {
-    $support_link =<<<SUPPORT_LINK
-&nbsp;|&nbsp;<a href="http://prettylinkpro.com">Premium Support</a>
-SUPPORT_LINK;
-  }
-    
+    $support_link = "&nbsp;|&nbsp;<a href=\"http://prettylinkpro.com\">" . __('Upgrade to Pro') . '</a>';
 ?>
-<p style="font-size: 14px; font-weight: bold; float: right; text-align: right; padding-top: 0px; padding-right: 10px;">Connect with Pretty Link:&nbsp;&nbsp;<a href="http://twitter.com/blairwilli"><img src="<?php echo PRLI_URL; ?>/images/twitter_32.png" style="width: 24px; height: 24px;" /></a>&nbsp;<a href="http://www.facebook.com/pages/Pretty-Link/283252860401"><img src="<?php echo PRLI_URL; ?>/images/facebook_32.png" style="width: 24px; height: 24px;" /></a><br/>Get Help:&nbsp;&nbsp;<a href="http://blairwilliams.com/xba" target="_blank">Tutorials (UPDATED!)</a><?php echo $support_link; ?>&nbsp;|&nbsp;<a href="http://blairwilliams.com/work">One on One</a></p>
+<p style="font-size: 14px; font-weight: bold; float: right; text-align: right; padding-top: 0px; padding-right: 10px;"><?php _e('Connect'); ?>:&nbsp;&nbsp;<a href="http://twitter.com/blairwilli"><img src="<?php echo PRLI_URL; ?>/images/twitter_32.png" style="width: 24px; height: 24px;" /></a>&nbsp;<a href="http://www.facebook.com/pages/Pretty-Link/283252860401"><img src="<?php echo PRLI_URL; ?>/images/facebook_32.png" style="width: 24px; height: 24px;" /></a><br/><?php _e('Get Help'); ?>:&nbsp;&nbsp;<a href="http://blairwilliams.com/xba" target="_blank"><?php _e('Tutorials'); ?></a><?php echo $support_link; ?>&nbsp;|&nbsp;<a href="http://blairwilliams.com/work"><?php _e('One on One'); ?></a></p>
